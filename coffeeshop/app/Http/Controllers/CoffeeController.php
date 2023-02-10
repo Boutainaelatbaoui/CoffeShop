ac1c66859a6c612039a5ca83bc2d5ad1447e6cc3
Boutaina <boutaina.elatbaoui@gmail.com>
2023-02-10 14:23:54 +0100

    Added update coffee

--- coffeeshop/app/Http/Controllers/CoffeeController.php
+++ coffeeshop/app/Http/Controllers/CoffeeController.php
@@ -93,7 +93,37 @@ class CoffeeController extends Controller
      */
     public function update(Request $request, Coffee $coffee)
     {
-        //
+        $request->validate([
+            'coffee_name'    => 'required',
+            'coffee_size'    =>  'required',
+            'coffee_price'   =>  'required',
+            'coffee_detail'  =>  'required',
+            'coffee_category'=>  'required',
+            'coffee_image'   =>  'required|image|mimes:jpg,png,jpeg,gif,svg'
+        ]);
+
+        $coffee_image = $request->hidden_coffee_image;
+
+        if($request->coffee_image != '')
+        {
+            $coffee_image = time() . '.' . request()->coffee_image->getClientOriginalExtension();
+            request()->coffee_image->move(public_path('img'), $coffee_image);
+
+            $coffee = Coffee::find($request->hidden_id);
+
+            $coffee->coffee_name     = $request->coffee_name;
+            $coffee->coffee_size     = $request->coffee_size;
+            $coffee->coffee_price    = $request->coffee_price;
+            $coffee->coffee_detail   = $request->coffee_detail;
+            $coffee->coffee_category = $request->coffee_category;
+            $coffee->coffee_image    = $coffee_image;
+
+            $coffee->save();
+
+            return redirect()->route('coffee.index')->with('success', 'Coffee updated successfully');
+
+
+        }
     }
 
     /**
