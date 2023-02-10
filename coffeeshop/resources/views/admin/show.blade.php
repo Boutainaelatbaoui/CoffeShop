eae83151267baf18b5c3cd5050a6f4994448072e
Boutaina <boutaina.elatbaoui@gmail.com>
2023-02-10 10:45:06 +0100

    Show specific item from the table

--- coffeeshop/resources/views/admin/show.blade.php
+++ coffeeshop/resources/views/admin/show.blade.php
@@ -5,18 +5,18 @@
     <div class="row d-flex justify-content-center">
         <div class="col-6">
             <div class="card card-menu btn bg-white mb-3 p-0">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="{{ asset('img/' . $coffee->coffee_image) }}" class="card-img-top" width="80" alt="...">
                 <div class="card-body">
-                    <h4 class="card-title fw-bolder">Card title</h4>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <h4 class="card-title fw-bolder">{{ $coffee->coffee_name }}</h4>
+                    <p class="card-text">{{ $coffee->coffee_detail }}</p>
                 </div>
                 <ul class="list-group list-group-flush text-start">
-                    <li class="list-group-item"><span class="fw-bold">Size: </span></li>
-                    <li class="list-group-item"><span class="fw-bold">Category: </span></li>
-                    <li class="list-group-item"><span class="fw-bold">Price: </span></li>
+                    <li class="list-group-item"><span class="fw-bold">Size: </span>{{ $coffee->coffee_size }}</li>
+                    <li class="list-group-item"><span class="fw-bold">Category: </span>{{ $coffee->coffee_category }}</li>
+                    <li class="list-group-item"><span class="fw-bold">Price: </span>{{ $coffee->coffee_price }}</li>
                 </ul>
                 <div class="card-body">
-                    <a href="#" class="card-link btn btn-menu">Order Now</a>
+                    <a href="{{route('coffee.index')}}" class="card-link btn btn-menu">View All</a>
                 </div>
             </div>
         </div>
